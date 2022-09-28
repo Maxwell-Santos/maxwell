dd0a711fc0075025d3b00c751fbee0bff7a05b83
Maxwell Alves Dos Santos <maxdsantos@simpress.com.br>
2022-09-27 21:43:59 -0300

    ajuste

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -9,7 +9,7 @@ interface StackProps {
 function Stack({ date, place, title, primary, id }: StackProps) {
   return (
 
-    <a href={`https://www.dio.me/certificate/${id}/`} target={"_blank"} rel="noopener">
+    <a href={`https://www.dio.me/certificate/${id}/`} target={"_blank"} rel="noreferrer" >
       <li
         className={`
       ${primary ? "bg-[#0055ff98]" : "bg-cards"}
